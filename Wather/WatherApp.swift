b2dbd238c39d4ef368fa18bf975f794fec492837
leonardo <leonardoazevedomoreiraa@gmail.com>
2023-05-30 12:05:07 -0300

    Initial Commit

---
+++ Wather/WatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatherApp.swift
+//  Wather
+//
+//  Created by leonardo Moreira on 30/05/23.
+//
+
+import SwiftUI
+
+@main
+struct WatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
